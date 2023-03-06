79f1ca4c4f25ef3693588cefeeece2939da45914
3611l <61923704+3611l@users.noreply.github.com>
2023-03-06 18:10:53 +0800

    Update offsets.h

--- apex_dma/offsets.h
+++ apex_dma/offsets.h
@@ -5,11 +5,11 @@
 
 #if VERSION == STEAM
 
-#define OFFSET_ENTITYLIST 0x1b37938 //cl_entitylist
-#define OFFSET_LOCAL_ENT 0x01ee8b70 + 0x8 //LocalPlayer  might be moved to  AVC_GameMovement
-#define OFFSET_NAME_LIST 0xbc9ec10 //NameList
-#define OFFSET_THIRDPERSON 0x01b1c6d0 + 0x6c //thirdperson_override + 0x6c
-#define OFFSET_TIMESCALE 0x014d2910 //host_timescale
+#define OFFSET_ENTITYLIST 0x1b37a78 //cl_entitylist
+#define OFFSET_LOCAL_ENT 0x01ee8cb0 + 0x8 //LocalPlayer  might be moved to  AVC_GameMovement
+#define OFFSET_NAME_LIST 0xbcc7e50 //NameList
+#define OFFSET_THIRDPERSON 0x01b1c810 + 0x6c //thirdperson_override + 0x6c
+#define OFFSET_TIMESCALE 0x014d2a40 //host_timescale
 
 #define OFFSET_TEAM 0x044c //m_iTeamNum
 #define OFFSET_HEALTH 0x043c //m_iHealth
@@ -25,15 +25,11 @@
 #define OFFSET_YAW 0x22b4 - 0x8 //m_currentFramePlayer.m_ammoPoolCount - 0x8
 
 #define OFFSET_LIFE_STATE 0x0798 //m_lifeState, >0 = dead
-#define OFFSET_BLEED_OUT_STATE 0x2740 //m_bleedoutState, >0 = knocked
+#define OFFSET_BLEED_OUT_STATE 0x2738 //m_bleedoutState, >0 = knocked
 
 #define OFFSET_ORIGIN 0x014c //m_vecAbsOrigin - 3rd offset after the first int and vector
-
-
 #define OFFSET_BONES 0x0e98 + 0x48 //m_nForceBone + 0x48
 #define OFFSET_STUDIOHDR 0x10f0 //CBaseAnimating!m_pStudioHdr
-
-
 #define OFFSET_AIMPUNCH 0x24b0 //m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle, first one
 #define OFFSET_CAMERAPOS 0x1f48 //CPlayer!camera_origin
 #define OFFSET_VIEWANGLES 0x25ac - 0x14 //m_ammoPoolCapacity - 0x14
@@ -42,19 +38,19 @@
 #define OFFSET_OBSERVING_TARGET 0x34e0 //m_hObserverTarget
 
 #define OFFSET_MATRIX 0x11a210 //ViewMatrix
-#define OFFSET_RENDER 0x7677280 //ViewRender
+#define OFFSET_RENDER 0x7677d20 //ViewRender
 
 #define OFFSET_WEAPON 0x1a1c //m_latestPrimaryWeapons
 #define OFFSET_BULLET_SPEED 0x1f18 //CWeaponX!m_flProjectileSpeed
 #define OFFSET_BULLET_SCALE 0x1f20 //CWeaponX!m_flProjectileScale
-#define OFFSET_ZOOM_FOV 0x16c0 + 0xb8 //m_playerData + m_curZoomFOV
+#define OFFSET_ZOOM_FOV 0x16c0 + 0x00b8 //m_playerData + m_curZoomFOV
 #define OFFSET_AMMO 0x1644 //m_ammoInClip first offset 
 
 #define OFFSET_ITEM_GLOW 0x02c0 //m_highlightFunctionBits
 #define OFFSET_ITEM_ID 0x1628 // item id?
 #define OFFSET_MODELNAME 0x0030 // m_ModelName
 #define OFFSET_M_CUSTOMSCRIPTINT 0x1648 //m_customScriptInt
-#define OFFSET_MINIMAP_SCALE 0x45f8 //m_minimapTargetZoomScale, float
+#define OFFSET_MINIMAP_SCALE 0x4620 //m_minimapTargetZoomScale, float
 
 #define OFFSET_GLOW_T1 0x262 //16256 = enabled, 0 = disabled
 #define OFFSET_GLOW_T2 0x2dc //1193322764 = enabled, 0 = disabled
@@ -64,7 +60,7 @@
 #define GLOW_COLOR_R 0x1D0 
 #define GLOW_COLOR_G 0x1D4 
 #define GLOW_COLOR_B 0x1D8
-#define GLOW_START_TIME 0x2c4 //m_playerFloatLookStartTime=0x02c4
+#define GLOW_START_TIME 0x02cc //m_playerFloatLookStartTime=0x02c4
 
 
 #define OFFSET_GLOW_ENABLE_GLOW_CONTEXT                 OFFSET_GLOW_ENABLE // Script_Highlight_SetCurrentContext
