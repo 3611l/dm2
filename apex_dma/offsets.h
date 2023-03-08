ef4906ab804fba658e9fb44e0e134ac6a236143f
3611l <61923704+3611l@users.noreply.github.com>
2023-03-08 12:45:59 +0800

    Update offsets.h

--- apex_dma/offsets.h
+++ apex_dma/offsets.h
@@ -5,10 +5,10 @@
 
 #if VERSION == STEAM
 
-#define OFFSET_ENTITYLIST 			0x1b37a78         //cl_entitylist
-#define OFFSET_LOCAL_ENT 			0x01ee8cb0 + 0x8  //AVC_GameMovement + 0x8
-#define OFFSET_NAME_LIST 			0xbcc7e50         //NameList
-#define OFFSET_THIRDPERSON 			0x01b1c810 + 0x6c //thirdperson_override + 0x6c
+#define OFFSET_ENTITYLIST 			0x1b37b18         //cl_entitylist
+#define OFFSET_LOCAL_ENT 			0x01ee8d50 + 0x8  //AVC_GameMovement + 0x8
+#define OFFSET_NAME_LIST 			0xbcc7ec0         //NameList
+#define OFFSET_THIRDPERSON 			0x01b1c8b0 + 0x6c //thirdperson_override + 0x6c
 #define OFFSET_TIMESCALE 			0x014d2a40        //host_timescale
 
 #define OFFSET_TEAM 				0x044c            //m_iTeamNum
@@ -19,7 +19,7 @@
 #define OFFSET_NAME				0x0589            //m_iName
 #define OFFSET_SIGN_NAME			0x0580            //m_iSignifierName
 #define OFFSET_ABS_VELOCITY         		0x0140            //m_vecAbsVelocity
-#define OFFSET_VISIBLE_TIME         		0x1a78            //CPlayer!lastVisibleTime
+#define OFFSET_VISIBLE_TIME         		0x1a78            //CPlayer!lastVisibleTime-0
 #define OFFSET_ZOOMING      			0x1c51            //m_bZooming
 #define OFFSET_THIRDPERSON_SV      		0x36c8            //m_thirdPersonShoulderView
 #define OFFSET_YAW                		0x22b4 - 0x8      //m_currentFramePlayer.m_ammoPoolCount - 0x8
@@ -39,7 +39,7 @@
 #define OFFSET_OBSERVING_TARGET		        0x34e0            //m_hObserverTarget
 
 #define OFFSET_MATRIX                           0x11a210          //ViewMatrix
-#define OFFSET_RENDER                           0x7677d20         //ViewRender
+#define OFFSET_RENDER                           0x7677db8         //ViewRender
 
 #define OFFSET_WEAPON			        0x1a1c            //m_latestPrimaryWeapons
 #define OFFSET_BULLET_SPEED                     0x1f18            //CWeaponX!m_flProjectileSpeed
